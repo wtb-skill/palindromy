1a85e3fbe375b3a35305aa23fdc69745da2b8659
Adam <adambaldyga@gmail.com>
2023-09-27 17:25:16 +0200

    Add TODO list

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # Zaprogramuj funkcję, która przyjmuje jeden argument (typu string) i zwraca wartość boolean: True/False,
 # mówiącą, czy podany tekst jest palindromem.
 
+# TODO 1️: Write the main function.
+# TODO 2: Check if the input is a string.
+# TODO 3️: Add docstring, check typing.
+
