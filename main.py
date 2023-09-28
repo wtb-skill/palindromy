cccf1fa23fcc3173c27566d54a4d5f6569a15776
Adam <adambaldyga@gmail.com>
2023-09-28 09:06:22 +0200

    Add check_input(), modify_word(), Modify palindrome()

--- main.py
+++ main.py
@@ -9,28 +9,58 @@ def check_if_palindrome(word: str) -> bool:
     """
     length = len(word)
     for index in range(0, length//2):
-        if word[index] == word[length - 1 - index]:
+        if word[index].lower() == word[length - 1 - index].lower():
             continue
         else:
             return False
     return True
 
 
+def check_input(user_input: str) -> str:
+    """
+    Checks what is the type of the string: is it a word, a number, a phrase or uncategorized.
+    :param user_input: any str.
+    :return: the type as a string.
+    """
+    if user_input.isalpha():
+        return "word"
+    if user_input.isdigit():
+        return "number"
+    if any(char.isalnum() or char.isspace() for char in user_input):
+        return "phrase"
+    return "uncategorized input"
+
+
+def modify_word(word: str) -> str:
+    """
+    Removes whitespace, space and punctuation from the user input.
+    :param word: any str.
+    :return: modified input.
+    """
+    punctuation = [' ', ',', '.', '!', '?']
+    word = word.strip()      # Remove leading and trailing whitespace
+    for sign in punctuation:      # Remove punctuation
+        word = word.replace(sign, "")
+    return word
+
+
 def palindrome() -> None:
     """
-    Prints a message if a given word is a palindrome.
+    Prints a message if a given string is a palindrome or not.
     :return: print message.
     """
-    user_word = input("Please enter a word: ")
-    if check_if_palindrome(word=user_word):
-        print(f"The word {user_word} is a palindrome.")
+    user_string = input("Please enter a word: ")
+    input_type = check_input(user_input=user_string)
+    modified_string = modify_word(user_string)
+    if check_if_palindrome(word=modified_string):
+        print(f"The {input_type} '{user_string}' is a palindrome.")
     else:
-        print(f"The word {user_word} is not a palindrome.")
+        print(f"The {input_type} '{user_string}' is not a palindrome.")
 
 
 palindrome()
 
-# TODO 1✔️: Write the main function.
-# TODO 2: Check if the input is a string.
-# TODO 3️: Add docstring, check typing.
+# TODO 1️: ✔ Write the main function.
+# TODO 2: ✔ Check if the input is a string.
+# TODO 3️: ✔ Add docstring, check typing.
 
